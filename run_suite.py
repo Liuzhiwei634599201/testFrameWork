84085493597523392a8a129ae601358692cb28fa
Liuzhiwei634599201 <62093947+Liuzhiwei634599201@users.noreply.github.com>
2020-03-28 15:34:20 +0800

    Update run_suite.py

--- run_suite.py
+++ run_suite.py
@@ -14,7 +14,7 @@ suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestIHRMLogin))
 # 使用runner来运行测试套件
 # 定义测试报告的文件名称
-reportname = app.BASE_DIR + "/report/report.html"
+reportname = app.BASE_DIR + "/report/ihrm.html"
 
 with open(reportname, 'wb') as f:
     # 初始化runner
